cc1ddaf53aaa48b0550cd3b7bd8da10d090cc39b
Amanuel85 <40584879+Avunue@users.noreply.github.com>
2021-03-23 20:49:09 -0500

    update

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -1,4 +1,58 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D2";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+//See tests.js for behavior
+function nameString(obj) {
+   let arr = [];
+ 
+if (obj.length === 0){
+    return "object has no properties"
 }
+
+    for(let key in obj){
+        arr.push(key); 
+      }
+         arr.sort();
+        let newarr = arr.join(".");
+            return newarr;}
+    
+
+function nameString1(obj) {
+ 
+if (obj === null){
+return "null argument"}
+
+else {return "no object passed"}
+}
+                            
+  
+
+    
+
+
+// function nameString(obj){
+//     for (let key in obj){
+//         if (key === null){
+//         return "no object passed" }}
+// }
+
+/*
+function test (obj){
+    let arr = []
+    let newarr;
+    for(let key in obj){
+      arr.push(key)
+      
+    }
+arr.sort()
+let newarr = arr.join(".")
+return newarr}
+*/
+
+// let obj = {a:5, z:6, m:8}
+// for (let key in obj)
+// {
+//     console.log(key)
+// }
+
